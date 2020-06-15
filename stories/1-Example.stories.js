96cf4db0b5a9526a79b3a2a8c15d183b6781226d
Ali Erenkara <alierenkara@gmail.com>
2020-06-15 23:09:22 +0300

    storybook
    storybook options and stories added

---
+++ stories/1-Example.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Example from '../components/example'
+
+export default {
+  title: 'Example',
+  component: Example
+}
+
+export const ItalicText = () => <Example text={'Example Component'} />
